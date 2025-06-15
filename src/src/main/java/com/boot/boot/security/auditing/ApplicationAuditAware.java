b72d25059ed04b8aa789d4310de4fb61e5bf6a3d
Fabio Souza Miranda <fabiosouza21777@gmail.com>
2025-06-15 11:31:36 -0300

    config jwt

---
+++ src/src/main/java/com/boot/boot/security/auditing/ApplicationAuditAware.java
@@ -0,0 +1,31 @@
+package com.boot.boot.security.auditing;
+
+import com.boot.boot.security.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.NonNullApi;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    @NonNull
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication =
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication();
+        if (authentication == null ||
+            !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken
+        ) {
+            return Optional.empty();
+        }
+
+        User userPrincipal = (User) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());
+    }
+}
---
+++ src/src/main/java/com/boot/boot/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.boot.boot.security.token;
+
+public enum TokenType {
+  BEARER
+}
